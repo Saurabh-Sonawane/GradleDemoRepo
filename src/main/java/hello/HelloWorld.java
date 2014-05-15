d4b7af031ebad6d574743d75ed5114fe744728e3
Saurabh <saurabh.sonawane@cognizant.com>
2014-05-15 18:11:24 +0530

    comment added

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,6 +1,7 @@
 package hello;
 
 public class HelloWorld {
+//Main Method
   public static void main(String[] args) {
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
